5df57c0ebcf3510835822c648b929039d40fcd66
Ryan Cockram <miaz@Ryans-MacBook-Pro.local>
2023-07-05 15:04:34 +0200

    add header component

---
+++ src/components/Header.svelte
@@ -0,0 +1,7 @@
+<div class={"w-full p-5"}>
+    <div class={"bg-base-300 h-16 rounded-lg flex shadow-lg"}>
+        <h1 class={"pink-first-letter lobster m-auto text-xl"}>
+            1337 Code Project
+        </h1>
+    </div>
+</div>
