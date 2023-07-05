e1e2c5d906e20d66314c11e82b2ce4d28fb7e64b
Ryan Cockram <miaz@Ryans-MacBook-Pro.local>
2023-07-05 22:33:34 +0200

    add responsive styling to usercards

--- src/components/UserCard.svelte
+++ src/components/UserCard.svelte
@@ -11,18 +11,18 @@
 </script>
 
 <!--Component which handles displaying of user data - utilizes $$props.class to handle any additional classes from params-->
-<div class={`flex flex-col h-96 rounded-lg shadow-lg scale-100 hover:scale-105 hover:transition-all transition-all duration-500 hover:duration-200 ${$$props.class} overflow-hidden relative select-none`}>
-    <Img {src} class={"z-10 absolute rounded-lg"} alt={`Image of ${userInfo.name}`}/>
-    <div class={"z-20 text-primary-content mt-auto mx-2 mb-2 rounded-lg bg-base-100 border-primary-focus border-2 h-24 p-2 shadow-lg flex flex-row"}>
+<div class={`flex flex-col xl:h-96 md:h-80 h-64 rounded-lg shadow-lg scale-100 hover:scale-105 hover:transition-all transition-all duration-500 hover:duration-200 ${$$props.class} overflow-hidden relative select-none`}>
+    <Img {src} class={"z-10 absolute lg:h-auto xl:w-auto w-full h-full rounded-lg"} alt={`Image of ${userInfo.name}`}/>
+    <div class={"z-20 lg:text-base text-xs text-primary-content mt-auto mx-2 mb-2 rounded-lg bg-base-100 border-primary-focus border-2 lg:h-20 h-16 p-2 shadow-lg flex flex-row"}>
         <div class={"flex flex-col w-3/4"}>
             <div class={"mt-auto text-primary-content font-bold wix-madefor-text"}>
                 {userInfo.name}
             </div>
-            <div class={"text-sm mb-auto text-primary-content open-sans"}>
+            <div class={"text-xs lg:text-sm opacity-70 mb-auto text-primary-content open-sans"}>
                 {userInfo.office ?? userInfo.orgUnit}
             </div>
         </div>
-        <div class={"ml-auto shrink flex flex-col justify-around mr-2"}>
+        <div class={"ml-auto shrink flex flex-row lg:flex-col lg:justify-around my-auto space-x-2 lg:space-x-0 lg:space-y-2 mr-2"}>
             {#if userInfo.github}
                 <a target={"_blank"} referrerpolicy={"no-referrer"} href={"https://github.com" + userInfo.github} class={"group"}>
                     <Fa class={"text-xl transition-all group-hover:text-[whitesmoke] group-active:text-primary-content"} icon={faGithub} />
