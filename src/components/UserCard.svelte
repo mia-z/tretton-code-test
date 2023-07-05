d6c3b73063eca68268f4e8ce75b6c2fbf36f58d9
Ryan Cockram <miaz@Ryans-MacBook-Pro.local>
2023-07-05 23:52:30 +0200

    add transition to cards

--- src/components/UserCard.svelte
+++ src/components/UserCard.svelte
@@ -2,6 +2,7 @@
     import Fa from "svelte-fa";
     import { faGithub, faTwitter, faLinkedin, faStackOverflow } from "@fortawesome/free-brands-svg-icons";
     import Img from "@zerodevx/svelte-img";
+	import { fade } from "svelte/transition";
 
     export let userInfo: User;
 
@@ -11,7 +12,7 @@
 </script>
 
 <!--Component which handles displaying of user data - utilizes $$props.class to handle any additional classes from params-->
-<div class={`flex flex-col xl:h-96 md:h-80 h-64 rounded-lg shadow-lg scale-100 hover:scale-105 hover:transition-all transition-all duration-500 hover:duration-200 ${$$props.class} overflow-hidden relative select-none`}>
+<div transition:fade class={`flex flex-col xl:h-96 md:h-80 h-64 rounded-lg shadow-lg scale-100 hover:scale-105 hover:transition-all transition-all duration-500 hover:duration-200 ${$$props.class} overflow-hidden relative select-none`}>
     <Img {src} class={"z-10 absolute lg:h-auto xl:w-auto w-full h-full rounded-lg"} alt={`Image of ${userInfo.name}`}/>
     <div class={"z-20 lg:text-base text-xs text-primary-content mt-auto mx-2 mb-2 rounded-lg bg-base-100 border-primary-focus border-2 lg:h-20 h-16 p-2 shadow-lg flex flex-row"}>
         <div class={"flex flex-col w-3/4"}>
