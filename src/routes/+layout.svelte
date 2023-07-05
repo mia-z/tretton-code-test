ee90fc88b47385b1eb88922f9aed4e8d6a03c207
Ryan Cockram <miaz@Ryans-MacBook-Pro.local>
2023-07-05 13:58:47 +0200

    update tailwind and establish base styling

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,6 @@
+<script lang="ts">
+    import "$styles/tw.css";
+    import "$styles/base.scss";
+</script>
+
+<slot />
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./src/**.{svelte}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+  content: ["./src/**/*.svelte"],
+  darkMode: "class",
+	theme: {
+		extend: {
+			spacing: {
+				128: "32rem",
+				144: "36rem",
+			},
+		},
+	},
+  plugins: [require("daisyui")],
+	daisyui: {
+		themes: ["dark", "light"]
+	}
 }
 
