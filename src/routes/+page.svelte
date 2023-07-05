c5bd17b74e2d6a2c778223fde541a5bf60f1a585
Ryan Cockram <miaz@Ryans-MacBook-Pro.local>
2023-07-05 23:49:45 +0200

    refactor reactive statements; can filter by search term

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -15,14 +15,16 @@
 
     let queryFilter: string = "";
 
-    $: {
-        orderBy === "Location" ?
-            usersToShow = $UsersStore.sort((a, b) => a.office?.localeCompare(b.office)) :
-            usersToShow = $UsersStore.sort((a, b) => a.name?.localeCompare(b.name));
-    }
+    $: orderBy === "Location" ?
+        usersToShow = $UsersStore.sort((a, b) => a.office?.localeCompare(b.office)) :
+        usersToShow = $UsersStore.sort((a, b) => a.name?.localeCompare(b.name));
 
-    $: {
-        usersToShow = $UsersStore.filter(x => filteredCities.includes(x.office))
+    $: usersToShow = $UsersStore.filter(x => filteredCities.includes(x.office));
+    
+    $: if (queryFilter.length > 2) {
+        usersToShow = $UsersStore.filter(x => x.name.toLowerCase().includes(queryFilter.toLowerCase()));
+    } else {
+        usersToShow = $UsersStore;
     }
 
     onMount(() => {
@@ -33,45 +35,47 @@
 
 
 <div class={"container mx-auto flex flex-col p-2"}>
-    <div class={"w-full rounded-lg mb-5 p-4 flex flex-col gap-y-2 bg-base-300 open-sans"}>
+    <div class={"w-full rounded-lg mb-5 flex flex-col p-4 f bg-base-300 open-sans"}>
         <div class={"text-lg text-center lobster"}>
             Search and filter
         </div>
-        <div class={"form-control"}>
-            <input id={"free-search-input"} class={"input input-bordered input-primary muted-placeholder"} placeholder={"Enter a search term.."} />
-        </div>
-        <div class={"flex flex-col bg-base-100 p-2 rounded-lg"}>
-            <div class={"bold mx-auto"}>
-                Sort by
+        <div class={"flex lg:gap-x-2 lg:flex-row lg:flex-wrap flex-col gap-y-2"}>
+            <div class={"form-control lg:w-full"}>
+                <input bind:value={queryFilter} id={"free-search-input"} class={"placeholder:text-center input input-bordered focus:input-primary muted-placeholder"} placeholder={"Enter a search term.."} />
             </div>
-            <div class={"grid grid-cols-2 grid-rows-2 grid-flow-row"}>
-                <div class={"col-span-1 text-center"}>
-                    Name
+            <div class={"lg:w-1/3 flex flex-col md bg-base-100 p-2 rounded-lg"}>
+                <div class={"font-bold lg:mb-2 mx-auto"}>
+                    Sort by
                 </div>
-                <div class={"col-span-1 text-center"}>
-                    Location
-                </div>
-                <div class={"col-span-1 form-control"}>
-                    <input type="radio" bind:group={orderBy} value={"Name"} name={"sort-radio"} class={"radio mx-auto checked:radio-primary input-xs"} />
-                </div>
-                <div class={"col-span-1 form-control"}>
-                    <input type="radio" bind:group={orderBy} value={"Location"} name={"sort-radio"} class={"radio mx-auto checked:radio-primary input-xs"} />
-                </div>
-            </div>
-        </div>
-        <div class={"flex flex-col bg-base-100 p-2 rounded-lg"}>
-            <div class={"bold mx-auto"}>
-                Filter Offices
-            </div>
-            <div class={"grid grid-cols-3 auto-rows-min auto-cols-min grid-flow-row gap-y-2"}>
-                {#each cities as city}
-                    <div class={"flex flex-col form-control"}>
-                        <label class={"text-center mb-1"} for={"checkbox-" + city}>
-                            {city}
-                        </label>
-                        <input type="checkbox" bind:group={filteredCities} value={city} id={"checkbox-" + city} />
+                <div class={"grid grid-cols-2 grid-rows-2 grid-flow-row"}>
+                    <div class={"col-span-1 text-center"}>
+                        Name
                     </div>
-                {/each}
+                    <div class={"col-span-1 text-center"}>
+                        Location
+                    </div>
+                    <div class={"col-span-1 form-control"}>
+                        <input type="radio" bind:group={orderBy} value={"Name"} name={"sort-radio"} class={"radio mx-auto checked:radio-primary input-xs"} />
+                    </div>
+                    <div class={"col-span-1 form-control"}>
+                        <input type="radio" bind:group={orderBy} value={"Location"} name={"sort-radio"} class={"radio mx-auto checked:radio-primary input-xs"} />
+                    </div>
+                </div>
+            </div>
+            <div class={"lg:grow flex flex-col bg-base-100 p-2 rounded-lg"}>
+                <div class={"font-bold lg:mb-2 mx-auto"}>
+                    Filter Offices
+                </div>
+                <div class={"grid lg:grid-cols-5 grid-cols-3 auto-rows-min auto-cols-min grid-flow-row gap-y-2"}>
+                    {#each cities as city}
+                        <div class={"flex flex-col form-control"}>
+                            <label class={"text-center mb-1"} for={"checkbox-" + city}>
+                                {city}
+                            </label>
+                            <input type="checkbox" bind:group={filteredCities} value={city} id={"checkbox-" + city} />
+                        </div>
+                    {/each}
+                </div>
             </div>
         </div>
     </div>
